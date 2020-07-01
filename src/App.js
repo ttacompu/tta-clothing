ecfed1553f848f89a01573e0c8f61c57f396398a
Thein Aung <ttacompu@gmail.com>
2020-07-01 12:19:20 -0400

    withRouter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Homepage from './pages/hompage/homepage.component';
+import {Route, Switch} from 'react-router-dom';
+
+const HatComponent = () =>(
+  (
+    <div>Hat Component</div>
+  )
+)
 
 function App() {
   return (
-    <div className="App">
-        <Homepage></Homepage>
-    </div>
+      <div className="App">
+          <Route exact={true} path="/" component={Homepage}></Route>
+          <Route path="/shop/hats" component={HatComponent}></Route>
+          
+      </div>
   );
 }
 
