96cfc406ba451b5e1341a7812de7ae408af4456a
Thein Aung <ttacompu@gmail.com>
2020-07-01 17:31:28 -0400

    added collection Item

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import Homepage from './pages/hompage/homepage.component';
 import {Route, Switch} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component'
 
-const HatComponent = () =>(
-  (
-    <div>Hat Component</div>
-  )
-)
+
 
 function App() {
   return (
       <div className="App">
           <Route exact={true} path="/" component={Homepage}></Route>
-          <Route path="/shop/hats" component={HatComponent}></Route>
+          <Route path="/shop" component={ShopPage}></Route>
           
       </div>
   );
