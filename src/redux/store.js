2c17560532928eec7e3185057e91b70c62a322fb
Thein Aung <ttacompu@gmail.com>
2020-07-20 22:25:00 -0400

    added store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import { createStore,  applyMiddleware } from 'redux'
 import {persistStore} from 'redux-persist';
 import logger from 'redux-logger';
 
-const middlewares = [logger]
+const middlewares = [];
+
+if(process.env.NODE_ENV === 'development'){
+    middlewares.push(logger);
+}
 
 
  export const store = createStore(rootReducer, applyMiddleware(...middlewares));
