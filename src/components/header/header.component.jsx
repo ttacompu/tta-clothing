cf6f7d15d2e4dbb4d6a383cf227f1a97fee30d8d
Thein Aung <ttacompu@gmail.com>
2020-08-08 16:21:26 -0400

    remove auth

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./header.styles.jsx";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-import { auth } from "../../firebase/firebase-utils";
 import { connect } from "react-redux";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
